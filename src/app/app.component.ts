d96ab8830664c1d1da73c28454f0a2f7c92e916f
Ben Nadel <ben@bennadel.com>
2022-09-04 10:37:09 -0400

    Working on a the demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+
+// Import core Angular modules.
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+// ----------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------- //
+
+interface Segment {
+	name: string;
+	weight: number;
+	count: number;
+	indicators: any[];
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+	standalone: true,
+	selector: "app-root",
+	imports: [ CommonModule ],
+	templateUrl: "./app.component.html",
+	styleUrls: [ "./app.component.less" ]
 })
 export class AppComponent {
-  title = 'Plate-Weight';
+
+	public segments: Segment[];
+	public total: number;
+
+	constructor() {
+
+		this.segments = [
+			{
+				name: "45",
+				weight: 45,
+				count: 0,
+				indicators: []
+			},
+			{
+				name: "25",
+				weight: 25,
+				count: 0,
+				indicators: []
+			},
+			{
+				name: "10",
+				weight: 10,
+				count: 0,
+				indicators: []
+			},
+			{
+				name: "5",
+				weight: 5,
+				count: 0,
+				indicators: []
+			},
+			{
+				name: "2.5",
+				weight: 2.5,
+				count: 0,
+				indicators: []
+			}
+		];
+		this.total = 0;
+
+	}
+
+	// ---
+	// PUBLIC METHODS.
+	// ---
+
+	public addPlate( segment: Segment ) : void {
+
+		segment.count++;
+		segment.indicators.push( segment.count );
+		this.setTotal();
+
+	}
+
+
+	public clearPlates() : void {
+
+		for ( var segment of this.segments ) {
+
+			segment.count = 0;
+			segment.indicators = [];
+
+		}
+
+		this.setTotal();
+
+	}
+
+
+	public removePlate( segment: Segment ) : void {
+
+		if ( segment.count ) {
+
+			segment.count--;
+			segment.indicators.pop();
+			this.setTotal();
+
+		}
+
+	}
+
+	// ---
+	// PRIVATE METHODS.
+	// ---
+
+	private setTotal() : void {
+
+		this.total = this.segments.reduce(
+			( reduction, segment ) => {
+
+				return( reduction + ( segment.weight * segment.count ) );
+
+			},
+			0
+		);
+
+	}
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// Import core Angular modules.
+import { bootstrapApplication } from "@angular/platform-browser";
 
-if (environment.production) {
-  enableProdMode();
-}
+// Import application modules.
+import { AppComponent } from "./app/app.component";
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// ----------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------- //
+
+bootstrapApplication( AppComponent );
